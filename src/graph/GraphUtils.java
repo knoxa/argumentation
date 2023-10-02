6a73c8a28ed470b0c11d99c4e230bcc03b3295a6
knoxa <swaffhamtown@outlook.com>
2023-10-02 10:26:25 +0100

    added induceGraph() method

--- src/graph/GraphUtils.java
+++ src/graph/GraphUtils.java
@@ -2,6 +2,7 @@ package graph;
 
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
@@ -10,6 +11,44 @@ import org.apache.commons.csv.CSVPrinter;
 import uk.ac.kent.dover.fastGraph.FastGraph;
 
 public class GraphUtils {
+
+	public static void induceGraph(FastGraph g, HashSet<Integer> nodes, HashSet<Integer> edges) {
+		
+		// Induce a subgraph of g from the given nodes and/or edges. This is done by adding 'missing'
+		// nodes and edges to the supplied sets. FastGraph.generateGraphFromSubgraph() can then be used to
+		// create a FastGraph object from the results.
+		
+		// Ensure that the node set has the nodes at each end of every supplied edge
+		
+		for (int e: edges) {
+			
+			nodes.add(g.getEdgeNode1(e));
+			nodes.add(g.getEdgeNode2(e));			
+		}
+		
+		// Add edges between any pair of nodes in the node set.
+		
+		boolean[] visitedEdges = new boolean[g.getNumberOfEdges()];
+		
+		//for every node in the graph
+		for(int n : nodes) {
+			//find what edges it connects to
+			int[] edgeConnections = g.getNodeConnectingEdges(n);
+			//for each of these connecting edges
+			for (int ce : edgeConnections) {
+				//find the other node
+				int cn = g.oppositeEnd(ce,n);
+				//if that node is also in the graph and this edge hasn't been visited already, then add the edge
+				if (nodes.contains(cn) && !visitedEdges[ce]) {
+					edges.add(ce);
+					visitedEdges[ce] = true;
+				}
+			}			
+		}
+
+		return;
+	}
+
 
  	public static void dumpGraph(FastGraph graph, String filename) {
  		
