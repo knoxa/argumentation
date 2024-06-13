2def29286c9c2a34f314c16d89474412deeb2cb4
knoxa <swaffhamtown@outlook.com>
2024-06-13 15:39:41 +0100

    a method to serialize to XML

--- src/order/Lattice.java
+++ src/order/Lattice.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
 public class Lattice<E, A> {
 
 	private List<Set<E>> extents;
 	private Map<Integer, Set<A>> attributes;
 	Map<Integer, Set<Integer>> preorder;
+	private String id = "_:" + String.valueOf(this.hashCode());
 
 	public List<Set<E>> getExtents() {
 		return extents;
@@ -34,4 +39,66 @@ public class Lattice<E, A> {
 		this.preorder = preorder;
 	}
 	
+	public void serializeLattice(ContentHandler ch) throws SAXException {
+		
+		AttributesImpl attr = new AttributesImpl();
+		attr.addAttribute("", "id",  "id", "String", this.id);
+		ch.startElement("", "lattice", "lattice", attr);
+		
+		for ( int x = 0; x < extents.size(); x++ ) {
+			
+			attr = new AttributesImpl();
+			attr.addAttribute("", "id",  "id", "Integer", String.valueOf(x));
+			ch.startElement("", "extent", "extent", attr);
+			
+			Set<A> attributeSet = attributes.get(x);
+			
+			if ( attributeSet != null ) {
+
+				for ( A attribute: attributeSet ) {
+					
+					ch.startElement("", "attribute", "attribute", null);
+					String value = attribute.toString();
+					ch.characters(value.toCharArray(), 0, value.length());
+					ch.endElement("", "attribute", "attribute");
+				}
+			}
+			
+			Set<E> objectSet = extents.get(x);
+			
+			if ( objectSet != null ) {
+				
+				for ( E object: objectSet ) {
+					
+					attr = new AttributesImpl();
+					ch.startElement("", "object", "object", attr);
+					String value = object.toString();
+					ch.characters(value.toCharArray(), 0, value.length());
+					ch.endElement("", "object", "object");
+				}
+			}
+						
+			ch.endElement("", "extent", "extent");
+		}
+		
+		ch.startElement("", "preorder", "preorder", null);
+		
+		for ( Integer fm: preorder.keySet() ) {
+			
+			Set<Integer> toSet = preorder.get(fm);
+			
+			for ( Integer to: toSet ) {
+				
+				attr = new AttributesImpl();
+				attr.addAttribute("", "from",  "from", "Integer", String.valueOf(fm));
+				attr.addAttribute("", "to",  "to", "Integer", String.valueOf(to));
+				ch.startElement("", "edge", "edge", attr);
+				ch.endElement("", "edge", "edge");
+			}
+		}
+		
+		ch.endElement("", "preorder", "preorder");
+
+		ch.endElement("", "lattice", "lattice");
+	}
 }
